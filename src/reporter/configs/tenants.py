7568b0caa44c0d119bc3f0a67a55f2cd139d459a
Gilbert Curbelo <gilbertcurbelo3@gmail.com>
2023-05-04 15:41:22 -0500

    hide emails

--- src/reporter/configs/tenants.py
+++ src/reporter/configs/tenants.py
@@ -1,16 +1,19 @@
+import os
+
+
 tenants = [
     {
         'name': 'designsafe',
         'proper_name': 'DesignSafe',
         'directories': ['CommunityData', 'MyData', 'MyProjects', 'NEES', 'NHERI-Published'],
-        'primary_receiver': 'jfreeze@tacc.utexas.edu',
-        'recipients': ['jfreeze@tacc.utexas.edu', 'jstubbs@tacc.utexas.edu', 'ajamthe@tacc.utexas.edu', 'gcurbelo@tacc.utexas.edu', 'hammock@tacc.utexas.edu']
+        'primary_receiver': os.environ.get('DS_PRIMARY_RECIPIENT', []),
+        'recipients': os.environ.get('DS_EMAIL_RECIPIENTS', [])
     },
     {
         'name': 'tacc',
         'proper_name': 'TACC',
         'directories': ['team_classify', 'Hobby-Eberly-Telesco', 'HETDEX-Work', 'Hobby-Eberly-Public', 'work'],
-        'primary_receiver': 'jstubbs@tacc.utexas.edu',
-        'recipients': ['jstubbs@tacc.utexas.edu', 'ajamthe@tacc.utexas.edu', 'gcurbelo@tacc.utexas.edu', 'hammock@tacc.utexas.edu']
+        'primary_receiver': os.environ.get('TACC_PRIMARY_RECIPIENT', []),
+        'recipients': os.environ.get('TACC_EMAIL_RECIPIENTS', [])
     },
 ]
