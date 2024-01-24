ac2dc95023db5ad4e105e5b8799c10d05affcc7b
Gilbert Curbelo <gilbertcurbelo3@gmail.com>
2024-01-24 12:27:57 -0600

    Helper functions for jupyterhub parser

---
+++ src/reporter/helpers/jupyterhub_usage_funcions.py
@@ -0,0 +1,37 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+from ..apps.services.jupyterhub.models import ParsedNginxFile
+
+
+def get_home_path(tenant):
+    """
+    Set home path based off of tenant
+
+    :return: home path of tenant
+    """
+    home_paths = {
+        'tacc': '/home/jovyan',
+        'designsafe': '/home/jupyter'
+    }
+    return home_paths.get(tenant, "")
+
+
+def get_symbolic_links(tenant):
+    """
+    Set symbolic link based off of tenant
+
+    :return: symbolic link(s) of tenant
+    """
+    symbolic_links = {
+        'tacc': {
+            '/home/jovyan/shared': '/home/jovyan/team_classify/shared'
+        },
+        'designsafe': {
+            '/home/jupyter/community': '/home/jupyter/CommunityData',
+            '/home/jupyter/mydata': '/home/jupyter/MyData',
+            '/home/jupyter/projects': '/home/jupyter/MyProjects'
+        }
+    }
+    return symbolic_links.get(tenant, {})
