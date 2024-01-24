80929fa25c951ed1cb83f05507bbd2cfe8f07dc3
Gilbert Curbelo <gilbertcurbelo3@gmail.com>
2024-01-24 12:16:12 -0600

    Change how services are saved

--- src/reporter/backend/views/ModelsHandler.py
+++ src/reporter/backend/views/ModelsHandler.py
@@ -1,52 +1,58 @@
-from ...apps.main.models import Service, Admin, Tenant, TenantDirectory, TenantRecipient
+from ...apps.main.models import Service, Admin, Tenant, \
+    TenantDirectory, TenantRecipient
 
 
 def save_service(service):
     ser = Service(
-        name = service['name']
+        name=service['name']
     )
     ser.save()
-    save_admins(ser, service['admins'])
+    if 'admins' in service and service['admins'] != []:
+        save_admins(ser, service['admins'])
 
-    if service['tenants'] != []:
+    if 'tenants' in service and service['tenants'] != []:
         save_tenants(ser, service['tenants'])
+
 
 def save_admins(ser, admins):
     for admin in admins:
         entry = Admin(
-            service = ser,
-            user = admin
+            service=ser,
+            user=admin
         )
         entry.save()
+
 
 def save_tenants(ser, tenants):
     for tenant in tenants:
         entry = Tenant(
-            service = ser,
-            name = tenant['name'],
-            primary_receiver = tenant['primary_receiver'],
-            proper_name = tenant['proper_name']
+            service=ser,
+            name=tenant['name'],
+            primary_receiver=tenant['primary_receiver'],
+            proper_name=tenant['proper_name']
         )
         entry.save()
 
-        if tenant['directories'] != []:
+        if 'directories' in tenant:
             save_directories(entry, tenant['directories'])
 
-        if tenant['recipients'] != []:
+        if 'recipients' in tenant:
             save_recipients(entry, tenant['recipients'])
+
 
 def save_directories(entry, directories):
     for directory in directories:
         dir = TenantDirectory(
-            tenant = entry,
-            directory = directory
+            tenant=entry,
+            directory=directory
         )
         dir.save()
+
 
 def save_recipients(entry, recipients):
     for recipient in recipients:
         rec = TenantRecipient(
-            tenant = entry,
-            recipient = recipient
+            tenant=entry,
+            recipient=recipient
         )
         rec.save()
